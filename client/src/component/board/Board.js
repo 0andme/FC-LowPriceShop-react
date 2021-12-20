216d043c2b6a0594861f4eb69fe7ed6861789e74
0andme <yu00mi97@gmail.com>
2021-12-20 13:15:26 +0900

    feat : Board
    state
    - boardTotal : 게시판 목록 총 개수
    - pageNum: 현재 게시판 페이지 번호
    - isInputOpen:게시판 글쓰기 버튼 클릭 여부
    
    변수
    -displayCnt : 화면에 출력될 게시판 개수
    
    useEffect
    : getBoardTotalCount 실행
    : 최초 렌더시 게시판 목록 총 개수를 가져옴
    
    useMemo
    : boardTotal값이 변경될때마다 계산된 페이지 개수 pageNavNum 도 변경
    
    function getBoardTotalCount
    : 게시판 총 개수 조회 api 호출
    : setBoardTotal 실행
    : 새로고침 버튼 클릭시 실행
    
    return
    - h2
    -현재 페이지 번호 출력
    - BoardList 컴포넌트
    -PageNav컴포넌트
    -controller 글쓰기&새로고침 버튼
    -BoardAdd 컴포넌트 : 글쓰기 버튼 ture시

--- client/src/component/board/Board.js
+++ client/src/component/board/Board.js
@@ -1,11 +1,73 @@
-import React from "react";
+import { useEffect, useMemo, useState } from "react";
+import axios from "axios";
+import BoardList from "./BoardList";
+import PageNav from "./PageNav";
+import { Button, Container } from "reactstrap";
+import BoardAdd from "./BoardAdd";
+function Board() {
+  // state
+  // 화면에 출력될 게시판 개수
+  const displayCnt = 10;
+  // 게시판 목록 총 개수
+  const [boardTotal, setBoardTotal] = useState(0);
+  //  현재 게시판 페이지 번호
+  const [pageNum, setPageNum] = useState(0);
+  // 게시판 글쓰기 버튼 클릭 여부
+  const [isInputOpen, setIsInputOpen] = useState(false);
+  // useEffect
+  // 게시판 목록 총 개수
+  useEffect(() => {
+    getBoardTotalCount();
+  }, []);
+  //useMemo
+  // 계산된 페이지 개수
+  const pageNavNum = useMemo(() => {
+    return Math.ceil(boardTotal / displayCnt);
+  }, [boardTotal]);
 
-const Board = (props) => {
   return (
-    <div>
+    <Container className="mainContent">
       <h2>게시판</h2>
-    </div>
+      {/* 현재 페이지 출력*/}
+      <div style={{ display: "flex ", justifyContent: "flex-start" }}>{`${
+        pageNum + 1
+      }/${pageNavNum}`}</div>
+      {/*  BoardList*/}
+      <BoardList displayCnt={displayCnt} pageNum={pageNum} />
+      {/* pageNav */}
+      <PageNav setPageNum={setPageNum} pageNavNum={pageNavNum} />
+      {/* controller -글쓰기 새로고침 */}
+      <div className="boardController">
+        <Button
+          onClick={() => {
+            setIsInputOpen((isOpen) => (isOpen = !isOpen));
+          }}
+        >
+          글쓰기
+        </Button>
+        <Button onClick={getBoardTotalCount}>새로고침</Button>
+      </div>
+      {isInputOpen && <BoardAdd></BoardAdd>}
+    </Container>
   );
-};
+
+  // 게시판 총 개수 조회 api-setBoardTotal
+  function getBoardTotalCount() {
+    axios
+      .post("/api/board?type=count", {})
+      .then((response) => {
+        try {
+          const cnt = response.data.json;
+          if (cnt) {
+            setBoardTotal(cnt[0].total_count);
+          }
+        } catch (error) {
+          console.log(error);
+          alert("게시판 개수를 가져오지 못했습니다");
+        }
+      })
+      .catch((error) => {});
+  }
+}
 
 export default Board;
