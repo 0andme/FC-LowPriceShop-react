7a140842d5a51f7f2e812bc21e54595e4229062b
0andme <yu00mi97@gmail.com>
2021-12-21 20:23:35 +0900

    feat :  구매 내역 페이징 기능 추가/ 기존의 전체 내역 출력 기능 제거

--- client/src/component/history/OrderList.js
+++ client/src/component/history/OrderList.js
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from "react";
 import { Table } from "reactstrap";
 import OrderListItem from "./OrderListItem";
 
-function OrderList({ userId }) {
+function OrderList({ userId, pageNum, displayCnt }) {
   const [orderList, setOrderList] = useState([]);
   useEffect(() => {
     getOrderList();
-  }, []);
+  }, [pageNum]);
   return (
     <div>
       <Table size="sm" hover>
@@ -28,7 +28,7 @@ function OrderList({ userId }) {
                   userId={userId}
                   index={index + 1}
                   item={item}
-                  key={item.product_id}
+                  key={index}
                 />
               );
             })}
@@ -38,9 +38,12 @@ function OrderList({ userId }) {
     </div>
   );
   function getOrderList() {
+    // 방법 1- 페이지별 구매 내역 조회
     axios
-      .post("/api/order?type=list", {
+      .post("/api/order?type=page", {
         user_id: userId,
+        param_length: displayCnt,
+        param_start: displayCnt * pageNum,
       })
       .then((res) => {
         try {
@@ -54,6 +57,23 @@ function OrderList({ userId }) {
       })
       .catch();
   }
+  // 방법 2- 구매 내역 전부 조회
+  //   axios
+  //     .post("/api/order?type=list", {
+  //       user_id: userId,
+  //     })
+  //     .then((res) => {
+  //       try {
+  //         const list = res.data.json;
+  //         if (list.length) {
+  //           setOrderList(list);
+  //         }
+  //       } catch {
+  //         alert("주문 내역을 가져오는 중 오류가 발생하였습니다");
+  //       }
+  //     })
+  //     .catch();
+  // }
 }
 
 export default OrderList;
