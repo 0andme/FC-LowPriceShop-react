e808cfaf9be2c88dbaddf34dd797535e8d584881
0andme <yu00mi97@gmail.com>
2021-12-21 20:17:58 +0900

    feat :  구매 내역 페이징 기능 추가/ 기존의 전체 내역 출력 기능 제거

--- client/src/component/history/History.js
+++ client/src/component/history/History.js
@@ -1,18 +1,52 @@
-import React from "react";
-import { Card, CardHeader, Container } from "reactstrap";
+import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
+import { Card, CardBody, CardHeader, Container } from "reactstrap";
 import OrderList from "./OrderList";
+import PageNav from "./PageNav";
 
 function History({ userId }) {
+  const displayCnt = 5;
+  const [orderTotalNum, setOrderTotalNum] = useState(0);
+  const [pageNum, setPageNum] = useState(0);
+  const pageNavNum = useMemo(() => {
+    return Math.ceil(orderTotalNum / displayCnt);
+  }, [orderTotalNum]);
+  useEffect(() => {
+    getOrderTotalNum();
+  }, []);
   return (
     <Container className="mainContent">
       <h2>주문 내역 조회</h2>
       <Card>
         <CardHeader>내 주문 정보</CardHeader>
         {/* 주문 정보 리스트 */}
-        <OrderList userId={userId} />
+        <CardBody>
+          <OrderList
+            pageNum={pageNum}
+            displayCnt={displayCnt}
+            userId={userId}
+          />
+          <PageNav pageNavNum={pageNavNum} setPageNum={setPageNum}></PageNav>
+        </CardBody>
       </Card>
     </Container>
   );
+  function getOrderTotalNum() {
+    axios
+      .post("/api/order?type=count", { user_id: userId })
+      .then((response) => {
+        try {
+          const cnt = response.data.json;
+          if (cnt) {
+            setOrderTotalNum(cnt[0].total_count);
+          }
+        } catch (error) {
+          console.log(error);
+          alert("작업 중 오류가 발생하였습니다.");
+        }
+      })
+      .catch((error) => {});
+  }
 }
 
 export default History;
---
+++ client/src/component/history/PageNav.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
+
+function PageNav({ pageNavNum, setPageNum }) {
+  // pageNavNum개의 PaginationItem 렌더를 위한 변수
+  const pageNavList = () => {
+    const navList = [];
+    for (let index = 0; index < pageNavNum; index++) {
+      navList.push(
+        <PaginationItem key={index}>
+          <PaginationLink
+            onClick={() => {
+              setPageNum(index);
+            }}
+          >
+            {index + 1}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    }
+    return navList;
+  };
+  return (
+    <div className="pageNav">
+      <Pagination>
+        <PaginationItem>
+          <PaginationLink
+            first
+            onClick={() => {
+              setPageNum(0);
+            }}
+          />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink
+            previous
+            onClick={() => {
+              setPageNum((num) => {
+                if (num === 0) {
+                  alert("첫 번째 페이지입니다");
+                  return 0;
+                } else return num - 1;
+              });
+            }}
+          />
+        </PaginationItem>
+        {pageNavList()}
+        <PaginationItem>
+          <PaginationLink
+            next
+            onClick={() => {
+              setPageNum((num) => {
+                if (num === pageNavNum - 1) {
+                  alert("마지막 페이지입니다");
+                  return num;
+                } else {
+                  return (num = num + 1);
+                }
+              });
+            }}
+          />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink
+            last
+            onClick={() => {
+              setPageNum(pageNavNum - 1);
+            }}
+          />
+        </PaginationItem>
+      </Pagination>
+    </div>
+  );
+}
+
+export default PageNav;
