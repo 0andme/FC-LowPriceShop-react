831a522088e70b27e124274669402a09c944b8a0
0andme <yu00mi97@gmail.com>
2021-12-21 17:29:51 +0900

    feat : 구매 내역 컴포넌트 History
    -구매 내역 리스트 컴포넌트 추가

--- client/src/component/history/History.js
+++ client/src/component/history/History.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Card, CardHeader, Container } from "reactstrap";
+import OrderList from "./OrderList";
 
-const History = (props) => {
+function History({ userId }) {
   return (
-    <div>
-      <h2>사용자 구매내역</h2>
-    </div>
+    <Container className="mainContent">
+      <h2>주문 내역 조회</h2>
+      <Card>
+        <CardHeader>내 주문 정보</CardHeader>
+        {/* 주문 정보 리스트 */}
+        <OrderList userId={userId} />
+      </Card>
+    </Container>
   );
-};
+}
 
 export default History;
