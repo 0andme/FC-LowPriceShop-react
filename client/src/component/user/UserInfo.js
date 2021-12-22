3be048ca8b33b547b81860f0250ed4753da02b90
0andme <yu00mi97@gmail.com>
2021-12-22 21:30:50 +0900

    feat : 기본 회원 정보 컴포넌트 추가

---
+++ client/src/component/user/UserInfo.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Card, Table } from "reactstrap";
+
+function UserInfo({ userData, userId }) {
+  return (
+    <Card
+      style={{
+        maxWidth: "750px",
+        margin: "0 auto",
+      }}
+    >
+      <Table>
+        <tbody>
+          <tr>
+            <td>이메일</td>
+            <td>{userData.user_email}</td>
+          </tr>
+          <tr>
+            <td>이름</td>
+            <td>{userData.user_name}</td>
+          </tr>
+          <tr>
+            <td>직업</td>
+            <td>{userData.user_org ? userData.user_org : "없음"}</td>
+          </tr>
+          <tr>
+            <td>전공</td>
+            <td>{userData.user_major ? userData.user_major : "없음"}</td>
+          </tr>
+          <tr>
+            <td>번호</td>
+            <td>
+              {userData.user_phone
+                ? userData.user_phone.replace(
+                    /(\d{3})(\d{4})(\d{4})/,
+                    "$1-$2-$3"
+                  )
+                : "없음"}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Card>
+  );
+}
+
+export default UserInfo;
