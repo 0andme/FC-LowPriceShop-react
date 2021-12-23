e5ac1a82e728f3ba4add1f3725e2fe58dcae8c45
0andme <yu00mi97@gmail.com>
2021-12-23 13:10:37 +0900

    feat : Home 컴포넌트 추가

---
+++ client/src/component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+function Home() {
+  return (
+    <Container className="mainContent">
+      <h2>홈</h2>
+    </Container>
+  );
+}
+
+export default Home;
