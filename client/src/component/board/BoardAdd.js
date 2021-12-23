4eca471a12c973185781f51bdcdd98187c9cb3f2
0andme <yu00mi97@gmail.com>
2021-12-23 17:25:00 +0900

    feat : 게시판 추가 입력 항목 추가  / 게시판 등록시 api호출 수정
    게시판 등록 성공시 입력 폼 닫기
    게시판 등록 성공시 전체 게시판 개수 다시 가져오기

--- client/src/component/board/BoardAdd.js
+++ client/src/component/board/BoardAdd.js
@@ -1,8 +1,10 @@
 import React, { useMemo, useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import axios from "axios";
-function BoardAdd() {
+function BoardAdd({ getBoardTotalCount, setIsInputOpen }) {
   //state
+  const [insertUser, setInsertUser] = useState("");
+  const [writePassword, setWritePassword] = useState("");
   // 게시판 제목
   const [boardTitle, setBoardTitle] = useState("");
   // 게시판 내용
@@ -15,56 +17,76 @@ function BoardAdd() {
       title: boardTitle,
       content: boardContent,
       id: "",
-      insert_user: "작성자Y",
+      insert_user: insertUser,
       view_count: 0,
-      write_password: 1234,
+      write_password: writePassword,
       insert_date: "",
     }),
-    [boardTitle, boardContent]
+    [boardTitle, boardContent, insertUser, writePassword]
   );
   return (
     <>
       <Form>
+        {/* 작성자 입력 폼 */}
+        <Label for="insertUser">작성자</Label>
+        <Input
+          id="insertUser"
+          placeholder="작성자"
+          type="text"
+          onChange={(e) => {
+            setInsertUser(e.target.value);
+          }}
+        ></Input>
+
+        {/* 비밀번호 입력 폼 */}
+        <Label for="writePassword">비밀번호</Label>
+        <Input
+          id="writePassword"
+          placeholder="비밀번호"
+          type="text"
+          onChange={(e) => {
+            setWritePassword(e.target.value);
+          }}
+        ></Input>
         {/* 제목 입력 폼 */}
-        <FormGroup>
-          <Label for="boardTitle">제목</Label>
-          <Input
-            id="boardTitle"
-            placeholder="제목"
-            type="text"
-            onChange={(e) => {
-              setBoardTitle(e.target.value);
-            }}
-          ></Input>
-        </FormGroup>
+        <Label for="boardTitle">제목</Label>
+        <Input
+          id="boardTitle"
+          placeholder="50자 이하의 제목"
+          type="text"
+          maxLength="50"
+          onChange={(e) => {
+            setBoardTitle(e.target.value);
+          }}
+        ></Input>
         {/* 내용 입력 폼 */}
-        <FormGroup>
-          <Label for="boardContent">내용</Label>
-          <Input
-            id="boardContent"
-            placeholder="내용"
-            type="textarea"
-            onChange={(e) => {
-              setBoardContent(e.target.value);
-            }}
-          ></Input>
-        </FormGroup>
+        <Label for="boardContent">내용</Label>
+        <Input
+          id="boardContent"
+          placeholder="내용"
+          type="textarea"
+          onChange={(e) => {
+            setBoardContent(e.target.value);
+          }}
+        ></Input>
       </Form>
       {/*  게시판 등록 버튼*/}
-      <Button onClick={addBoard}>게시판 등록</Button>
+      <Button style={{ marginTop: "10px" }} onClick={addBoard}>
+        게시판 등록
+      </Button>
       <div>{}</div>
     </>
   );
   // api - 게시판 등록 addBoard
   function addBoard() {
-    console.log(boardData);
     axios
       .post("/api/board?type=save", boardData)
       .then((response) => {
         try {
-          console.log(response);
+          alert("게시판 등록 성공");
+          setIsInputOpen(false);
+          getBoardTotalCount();
         } catch (error) {
-          console.log(error);
           alert("작업 중 오류가 발생하였습니다.");
         }
       })
