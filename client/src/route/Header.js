6f018117fcd2b9ce920666f0a5be1951d44141af
0andme <yu00mi97@gmail.com>
2021-12-23 13:12:41 +0900

    feat : userId 유무에 따라 라우터 링크 렌더

--- client/src/route/Header.js
+++ client/src/route/Header.js
@@ -4,6 +4,7 @@ import { Nav, Navbar, NavbarBrand, NavItem } from "reactstrap";
 
 class Header extends Component {
   render() {
+    const userId = this.props.userId;
     return (
       <header>
         <Navbar>
@@ -19,24 +20,45 @@ class Header extends Component {
           </NavbarBrand>
           {/* <NavbarToggler /> */}
           <Nav className="mr-auto" className="menu">
+            {!userId && (
+              <NavItem>
+                <NavLink to={"/login"}>로그인</NavLink>
+              </NavItem>
+            )}
+            {!userId && (
+              <NavItem>
+                <NavLink to={"/register"}>회원가입</NavLink>
+              </NavItem>
+            )}
             <NavItem>
               <NavLink to={"/board"}>게시판</NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink to={"/register"}>회원가입</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to={"/naverApi"}>상품등록</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to={"/product"}>상품목록</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to={"/cart"}>장바구니</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to={"/history"}>주문 내역</NavLink>
-            </NavItem>
+
+            {userId && (
+              <NavItem>
+                <NavLink to={"/user"}>마이페이지</NavLink>
+              </NavItem>
+            )}
+            {userId && (
+              <NavItem>
+                <NavLink to={"/naverApi"}>상품등록</NavLink>
+              </NavItem>
+            )}
+            {userId && (
+              <NavItem>
+                <NavLink to={"/product"}>상품목록</NavLink>
+              </NavItem>
+            )}
+            {userId && (
+              <NavItem>
+                <NavLink to={"/cart"}>장바구니</NavLink>
+              </NavItem>
+            )}
+            {userId && (
+              <NavItem>
+                <NavLink to={"/history"}>주문 내역</NavLink>
+              </NavItem>
+            )}
           </Nav>
         </Navbar>
       </header>
