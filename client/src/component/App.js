9c5e130d7556df80ea46714081cea662944301f6
0andme <yu00mi97@gmail.com>
2021-12-23 13:13:10 +0900

    feat : 홈/ 마이페이지 컴포넌트 연결

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -11,18 +11,28 @@ import Product from "../component/product/Product";
 import Cart from "../component/cart/Cart";
 import History from "../component/history/History";
 import SearchProduct from "./search/SearchProduct";
+import User from "./user/User";
+import Home from "./Home/Home";
 import { useState } from "react";
-function App() {
-  const [userId, setUserId] = useState("yu00mi97@gmail.com");
 
+function App() {
+  const [userId, setUserId] = useState("");
   return (
     <div className="App">
-      <Header />
-      <Route exact path="/" component={Login} />
-      <Route exact path="/login" component={Login} />
+      <Header userId={userId} />
+      <Route exact path="/" render={() => <Home />} />
+      <Route
+        exact
+        path="/login"
+        render={() => <Login setUserId={setUserId} />}
+      />
+
       <Route exact path="/register" component={Register} />
       <Route exact path="/naverApi" component={SearchProduct} />
       <Route exact path="/board" component={Board} />
+
+      <Route exact path="/user" render={() => <User userId={userId} />} />
+
       <Route path="/product" render={() => <Product userId={userId} />} />
       <Route path="/cart" render={() => <Cart userId={userId} />} />
       <Route path="/history" render={() => <History userId={userId} />} />
